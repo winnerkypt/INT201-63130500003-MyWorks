6e8874a39b829ef63b0207e4576554f169a11e28
winnerkypt <kanyapat.winnerkypt@mail.kmutt.ac.th>
2021-12-05 14:11:16 +0700

    add reverseMethod.js

--- assignment/ArrayMethod/10-sliceMethod.js
+++ assignment/ArrayMethod/10-sliceMethod.js
@@ -4,7 +4,7 @@ console.log(fruits.slice(1)) //[ 'orange', 'grape', 'banana', 'orange' ]
 console.log(fruits.slice(0,3))//[ 'mango', 'orange', 'grape' ]
 
 console.log(fruits.slice(-3))//[ 'grape', 'banana', 'orange' ]
-console.log(fruits.slice(-3,-1))
+console.log(fruits.slice(-3,-1))//[ 'grape', 'banana' ]
 
 console.log(fruits.slice(0,-3))//[ 'mango', 'orange' ]
 console.log(fruits.slice(-5,2))//[ 'mango', 'orange' ]
---
+++ assignment/ArrayMethod/11-reverseMethod.js
@@ -0,0 +1,5 @@
+const letter = ['a','b','c']
+
+console.log(letter) //[ 'a', 'b', 'c' ]
+console.log(letter.reverse()) //[ 'c', 'b', 'a' ]
+console.log(letter) //[ 'c', 'b', 'a' ]
