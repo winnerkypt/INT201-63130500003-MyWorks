c9a61a933632966adb08f319109f93a90dc7b541
winnerkypt <kanyapat.winnerkypt@mail.kmutt.ac.th>
2021-12-05 13:02:14 +0700

    add indexOfMethod.js

---
+++ assignment/ArrayMethod/07-indexOfMethod.js
@@ -0,0 +1,6 @@
+const fruits = ['mango','orange','grape','banana','orange']
+
+console.log(fruits.indexOf('orange')) //1
+console.log(fruits.indexOf('orange',0)) //1
+console.log(fruits.indexOf('orange',3)) //4
+console.log(fruits.indexOf('apple')) //-1
