52977b3b7340070c7ef80f72d1cc9a99851de19c
winnerkypt <kanyapat.winnerkypt@mail.kmutt.ac.th>
2021-12-05 13:57:29 +0700

    add sliceMethod.js

---
+++ assignment/ArrayMethod/10-sliceMethod.js
@@ -0,0 +1,10 @@
+const fruits = ['mango','orange','grape','banana','orange']
+
+console.log(fruits.slice(1)) //[ 'orange', 'grape', 'banana', 'orange' ]
+console.log(fruits.slice(0,3))//[ 'mango', 'orange', 'grape' ]
+
+console.log(fruits.slice(-3))//[ 'grape', 'banana', 'orange' ]
+console.log(fruits.slice(-3,-1))
+
+console.log(fruits.slice(0,-3))//[ 'mango', 'orange' ]
+console.log(fruits.slice(-5,2))//[ 'mango', 'orange' ]
